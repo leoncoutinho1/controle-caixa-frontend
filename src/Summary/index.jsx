2f1dd6281c5d818bea2e03362d050ca72f675c5f
leoncoutinho1 <leon.coutinho1@hotmail.com>
2020-08-24 14:02:28 -0300

    Add page to list operations

---
+++ src/Summary/index.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import OperationList from '../OperationList'
+
+const url = process.env.REACT_APP_API_URL
+
+export default class Summary extends Component {
+
+    constructor(props) {
+        super(props)
+        this.refresh()
+    }
+
+    state = {
+        flow: 'padaria',
+        list: []
+    }
+
+    setCashFlow = (e) => {
+        this.setState({
+            ...this.state,
+            flow: e.target.value
+        }, () => this.refresh())
+    }
+    refresh = () => {
+        if (this.state.flow === '*') {
+            axios.get(`${url}?sort=-createdAt&limit=50`)
+            .then(res => this.setState({
+                ...this.state,
+                list: res.data
+            }))
+        } else {
+            axios.get(`${url}?sort=-createdAt&limit=50&cashflow=${this.state.flow}`)
+            .then(res => this.setState({
+                ...this.state,
+                list: res.data
+            }))
+        }
+        
+    }
+ 
+    handleChange = () => {}
+
+    handleRemove = () => {}
+
+    render() {
+        return (
+            <>
+                <select name="inputCashFlow" id="inputCashFlow" value={this.state.flow} onChange={e => this.setCashFlow(e)}>
+                    <option value="">-- Caixa --</option>
+                    <option value="padaria">Padaria</option>
+                    <option value="mercearia">Mercearia</option>
+                    <option value="pessoal">Pessoal</option>
+                    <option value="*">Geral</option>
+                </select>
+                <OperationList
+                    list={this.state.list} />
+            </>
+            
+        )
+    }
+}
